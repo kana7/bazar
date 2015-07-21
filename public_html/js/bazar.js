c23c2bb4f136c4d94d0a3ddef58a23463a65e1db
Quentin Fonck <quentin.fonck@gmail.com>
2015-07-21 17:06:26 +0200

    nettoyage des fichiers dans le bazar

--- public_html/js/bazar.js
+++ public_html/js/bazar.js
@@ -142,17 +142,6 @@ function viewport() {
     }
     return {width: e[ a + 'Width' ], height: e[ a + 'Height' ]};
 }
-function getViewportClass() {
-    if (viewport().width >= 1170) {
-        return 'lgdesk';
-    } else if (viewport().width < 1172 && viewport().width >= 992) {
-        return 'desk';
-    } else if (viewport().width < 992 && viewport().width >= 680) {
-        return 'tab';
-    } else if (viewport().width < 680) {
-        return 'phone';
-    }
-}
 var waitForFinalEvent = (function () {
     var timers = {};
     return function (callback, ms, uniqueId) {
